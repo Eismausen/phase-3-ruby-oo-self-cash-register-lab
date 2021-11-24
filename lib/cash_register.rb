31040da9fa6c2c53a18455f5edb9750d0e22b34a
David Avery <eismausen@gmail.com>
2021-11-24 15:21:32 -0800

    commit tree 140d0b27d327d8a9bf6ccf2e3cc859bf0e49f120

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
 
+    def total
+        return @total
+    end
+
+    def add_item(title, price, quantity=1)
+        pass
+    end
+
+    def apply_discount
+        pass
+    end
+
+    def void_last_transaction
+        pass
+    end
+    
+end
